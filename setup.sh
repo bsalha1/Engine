67c9e8cc3cb7341ce2a80e8db2bb5e48e9f9c552
Bilal Salha <bsalha1@gmail.com>
2025-11-24 17:34:30 -0800

    Fix setup.sh again. Add vscode settings for clangd.

--- setup.sh
+++ setup.sh
@@ -2,22 +2,26 @@
 
 set -e
 
-sudo pacman -Sy clang-format
+base_dir="$(dirname $(realpath $0))"
+sudo pacman -Sy clang
 
 # Build GLFW library and headers.
 cd glfw
 cmake -S . -B build
 cd build
 make
+cd "$base_dir"
 
 # Build GLEW library and headers.
 cd glew/auto
 make
 cd ..
 make
+cd "$base_dir"
 
 # Build libglvnd library and headers.
 cd libglvnd
 ./autogen.sh
 ./configure
 sudo make install
+cd "$base_dir"
